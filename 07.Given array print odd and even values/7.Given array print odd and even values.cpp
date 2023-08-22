2757b26b78804ed21c6c9824dc9812762afd026b
chandu67567 <132892059+chandu67567@users.noreply.github.com>
2023-08-22 12:33:55 +0530

    Add files via upload

---
+++ 07.Given array print odd and even values/7.Given array print odd and even values.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int numbers[] = {5, 2, 8, 1, 9, 3};
+    int size = sizeof(numbers) / sizeof(numbers[0]);
+
+    printf("Odd values: ");
+    for (int i = 0; i < size; i++) {
+        if (numbers[i] % 2 != 0) {
+            printf("%d ", numbers[i]);
+        }
+    }
+    printf("\n");
+
+    printf("Even values: ");
+    for (int i = 0; i < size; i++) {
+        if (numbers[i] % 2 == 0) {
+            printf("%d ", numbers[i]);
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
+
